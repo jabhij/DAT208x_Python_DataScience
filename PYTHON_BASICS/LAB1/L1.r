17149cfac97868ea7c79eccb8b3c2814b9d4366c
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-13 19:22:15 +0530

    The Python Interface

---
+++ PYTHON_BASICS/LAB1/L1.r
@@ -0,0 +1,9 @@
+Instructions --
+
+Experiment in the IPython Shell; type 5 / 8, for example.
+Add another line of code to the Python script: print(7 + 10).
+Hit Submit Answer to execute the Python script and receive feedback.
+--------------------
+
+print(5 / 8)
+print(7 + 10)
