705608d8a6546e6b804015bd6df4e0c427ecb4be
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-13 22:45:00 +0530

    Variable Assignment

--- PYTHON_BASICS/LAB2/L1.r
+++ PYTHON_BASICS/LAB2/L1.r
@@ -1,1 +1,11 @@
+Instructions --
 
+Create a variable savings with the value 100.
+Check out this variable by typing print(savings) in the script.
+------------------
+
+# Create a variable savings
+savings = 100
+
+# Print out savings
+print(savings)
