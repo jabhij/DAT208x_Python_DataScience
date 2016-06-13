92eb4f4867a767bb252267e10ba7a3b0b7c8094a
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-13 19:24:53 +0530

    When to use Python?

---
+++ PYTHON_BASICS/LAB1/L2.r
@@ -0,0 +1,6 @@
+Python is a pretty versatile language. For what applications can you use Python?
+
+You want to do some quick calculations. 
+For your new business, you want to develop a database-driven website. 
+Your boss asks you to clean and analyze the results of the latest satisfaction survey. 
+All of the above. (CORRECT)
