d50bb7bc628cc8817ac1923cc98f219c0dce205e
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-13 19:57:50 +0530

    Any comments?

---
+++ PYTHON_BASICS/LAB1/L3.r
@@ -0,0 +1,10 @@
+Instructions --
+
+Above the print(7 + 10), add the comment # Addition works too.
+------------------
+
+# Just testing division
+print(5 / 8)
+
+# Addition works too.
+print(7 + 10)
