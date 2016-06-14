fc0937bec1d7adb0ea3f6c9bf3136c2f3c3083f3
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-15 00:58:28 +0530

    Guess the type

---
+++ PYTHON_BASICS/LAB2/L4.r
@@ -0,0 +1,9 @@
+type(a)
+We already went ahead and created three variables: a, b and c. You can use the IPython shell on the right to discover their type. 
+Which of the following options is correct?
+
+a is of type int, b is of type str, c is of type bool 
+a is of type float, b is of type bool, c is of type str
+a is of type float, b is of type str, c is of type bool (CORRECT)
+a is of type int, b is of type bool, c is of type str
+
