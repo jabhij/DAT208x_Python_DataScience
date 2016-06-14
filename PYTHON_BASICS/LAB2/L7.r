8b3865097f700bfb81d0e870aa759bf686ce6855
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-15 01:02:56 +0530

    Can Python handle everything?

---
+++ PYTHON_BASICS/LAB2/L7.r
@@ -0,0 +1,7 @@
+Have a look at the 4 Python expressions below. Which one of these will throw an error? You can always copy and paste this code in the 
+IPython Shell to find out!
+
+"I can add integers, like " + str(5) + " to strings." 
+"I said " + ("Hey " * 2) + "Hey!" 
+"The correct answer to this multiple choice exercise is answer number " + 2  (CORRECT)
+True + False
