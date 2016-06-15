fa5ce8cfd9db392a041bfed18195e7d8dd650e9f
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-15 19:38:42 +0530

    Subsetting lists of lists

---
+++ LIST/LAB2/L5.r
@@ -0,0 +1,15 @@
+x = [["a", "b", "c"], 
+     ["d", "e", "f"], 
+     ["g", "h", "i"]]
+x[2][0]
+x[2][:2]
+x[2] results in a list, that you can subset again by adding additional square brackets.
+
+What will house[-1][1] return? house, the list of lists that you created before, is already defined for you in the workspace. 
+You can experiment with it in the IPython Shell.
+-----------------
+
+A float: the kitchen area 
+A string: "kitchen" 
+A float: the bathroom area (CORRECT) 
+A string: "bathroom"
