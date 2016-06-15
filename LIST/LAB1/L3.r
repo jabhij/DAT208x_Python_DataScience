96e8882773fca0fa0658e17b470774ddb9c880d2
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-15 19:27:46 +0530

    Select the valid list

---
+++ LIST/LAB1/L3.r
@@ -0,0 +1,12 @@
+my_list = [el1, el2, el3]
+Can you tell which ones of the following lines of Python code are valid ways to build a list?
+
+A. [1, 3, 4, 2]
+B. [[1, 2, 3], [4, 5, 7]]
+C. [1 + 2, "a" * 5, 3]
+-------------
+
+A, B and C (CORRECT)
+B 
+B and C 
+C
