2883531a2a4e55f3350d794e72d628695e7edffc
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-16 15:08:16 +0530

    Help

---
+++ FUNCTIONS-PACKAGES/LAB1/L2.r
@@ -0,0 +1,6 @@
+Which of the following statements is true?
+
+complex() takes exactly two arguments: real and [, imag]. 
+complex() takes two arguments: real and imag. Both these arguments are required. 
+complex() takes two arguments: real and imag. real is a required argument, imag is an optional argument. (CORRECT)
+complex() takes two arguments: real and imag. If you don't specify imag, it is set to 1 by Python. 
