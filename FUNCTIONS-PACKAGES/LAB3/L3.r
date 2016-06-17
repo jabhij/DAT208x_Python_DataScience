2c775cdd67de6f587a15acda4e081648352886f4
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-17 22:00:23 +0530

    Different ways of Importing

---
+++ FUNCTIONS-PACKAGES/LAB3/L3.r
@@ -0,0 +1,6 @@
+Which import statement will you need in order to run the above code without an error?
+
+import scipy 
+import scipy.linalg 
+from scipy.linalg import my_inv 
+from scipy.linalg import inv as my_inv (CORRECT)
