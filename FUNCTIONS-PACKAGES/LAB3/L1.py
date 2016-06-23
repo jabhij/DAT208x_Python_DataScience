6b1becbdac2b89560ce58d2408a66ec1fa8279bf
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-23 20:09:30 +0530

    Import package

--- FUNCTIONS-PACKAGES/LAB3/L1.py
+++ FUNCTIONS-PACKAGES/LAB3/L1.py
@@ -1,9 +1,11 @@
+"""
 Instructions --
 
 Import the math package. Now you can access the constant pi with math.pi.
 Calculate the circumference of the circle and store it in C.
 Calculate the area of the circle and store it in A.
 ------------------
+"""
 
 # Definition of radius
 r = 0.43
