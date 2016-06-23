35d1de6007b260d5d248445a8a07ca64fc6cafec
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-23 20:05:20 +0530

    Familiar functions

--- FUNCTIONS-PACKAGES/LAB1/L1.py
+++ FUNCTIONS-PACKAGES/LAB1/L1.py
@@ -1,9 +1,11 @@
+"""
 Instructions --
 
 Use print() in combination with type() to print out the type of var1.
 Use len() to get the length of the list var1. Wrap it in a print() call to directly print it out.
 Use int() to convert var2 to an integer. Store the output as out2.
 -----------------
+"""
 
 # Create variables var1 and var2
 var1 = [1, 2, 3, 4]
