c6737c543c6566936d45ded0d2d6e5be1e17c522
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-25 21:09:30 +0530

    Subset and calculate

--- LIST/LAB2/L2.py
+++ LIST/LAB2/L2.py
@@ -1,9 +1,11 @@
+"""
 Instructions --
 
 Using a combination of list subsetting and variable assignment, create a new variable, eat_sleep_area, that contains the sum of the area 
 of the kitchen and the area of the bedroom.
 Print this new variable eat_sleep_area.
 ------------------
+"""
 
 # Create the areas list
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
