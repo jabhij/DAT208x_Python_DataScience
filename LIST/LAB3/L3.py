c8f9f5fcc528e5ee0c7fb4c5fc4b5035cb645c46
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-25 21:13:10 +0530

    Inner workings of lists

--- LIST/LAB3/L3.py
+++ LIST/LAB3/L3.py
@@ -1,8 +1,10 @@
+"""
 Instructions --
 
 Change the second command, that creates the variable areas_copy, such that areas_copy is an explicit copy of areas
 Now, changes made to areas_copy shouldn't affect areas. Hit Submit Answer to check this.
 ------------------
+"""
 
 # Create list areas
 areas = [11.25, 18.0, 20.0, 10.75, 9.50]
