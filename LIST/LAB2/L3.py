7c201d510a70443acfed8c45baf0cf1e64784709
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-25 21:10:02 +0530

    Slicing and dicing

--- LIST/LAB2/L3.py
+++ LIST/LAB2/L3.py
@@ -1,9 +1,11 @@
+"""
 Instructions --
 
 Use slicing to create a list, downstairs, that contains the first 6 elements of areas.
 Do a similar thing to create a new variable, upstairs, that contains the last 4 elements of areas.
 Print both downstairs and upstairs using print()
 ----------------
+"""
 
 # Create the areas list
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
