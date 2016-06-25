5c2b9ab1b975fe2cbf83559c2be135bed21067dd
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-25 21:12:07 +0530

    Replace list elements

--- LIST/LAB3/L1.py
+++ LIST/LAB3/L1.py
@@ -1,8 +1,10 @@
+"""
 Instructions --
 
 You did a miscalculation when determining the area of the bathroom; it's 10.50 square meters instead of 9.50. Can you make the changes?
 Make the areas list more trendy! Change "living room" to "chill zone".
 -------------------
+"""
 
 # Create the areas list
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
