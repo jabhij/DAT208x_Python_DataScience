3e0cad591625dc6714b02184ead7692106581fc2
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-29 19:59:37 +0530

    Numpy Side Effects

---
+++ NUMPY/LAB1/L5.py
@@ -0,0 +1,8 @@
+np.array([True, 1, 2]) + np.array([3, 4, False])
+Can you tell which code chunk builds the exact same Python data structure? The Numpy package is already imported as np, so you can 
+start experimenting in the IPython Shell straight away!
+
+np.array([True, 1, 2, 3, 4, False])  
+np.array([4, 3, 0]) + np.array([0, 2, 2])  (CORRECT)
+np.array([1, 1, 2]) + np.array([3, 4, -1]) 
+np.array([0, 1, 2, 3, 4, 5])
