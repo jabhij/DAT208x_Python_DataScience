f46e84eeac4ec141c06962e577d4af9e52580923
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-30 20:52:03 +0530

    Baseball data in 2D form

---
+++ NUMPY/LAB2/L2.py
@@ -0,0 +1,17 @@
+"""
+Instructions --
+
+Use np.array() to create a 2D Numpy array from baseball. Name it np_baseball.
+Print out the shape attribute of np_baseball.
+"""
+
+# baseball is available as a regular list of lists
+
+# Import numpy package
+import numpy as np
+
+# Create a 2D Numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out the shape of np_baseball
+print (np_baseball.shape)
