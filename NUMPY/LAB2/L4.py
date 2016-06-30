d19006dc1dd4701b037ee5ba974d1a5be74670ba
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-30 21:41:09 +0530

    2D Arithmetic

--- NUMPY/LAB2/L4.py
+++ NUMPY/LAB2/L4.py
@@ -6,3 +6,20 @@ You want to convert the units of height and weight. As a first step, create a Nu
 Multiply np_baseball with conversion and print out the result.
 """
 
+# baseball is available as a regular list of lists
+# update is available as 2D Numpy array
+
+# Import numpy package
+import numpy as np
+
+# Create np_baseball (3 cols)
+np_baseball = np.array(baseball)
+
+# Print out addition of np_baseball and update
+print (np_baseball + update)
+
+# Create Numpy array: conversion
+conversion = np.array([0.0254, 0.453592, 1])
+
+# Print out product of np_baseball and conversion
+print (np_baseball * conversion)
