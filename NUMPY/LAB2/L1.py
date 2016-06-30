8fa0050ad9071a610d98195cefe84917384d67ba
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-30 20:46:42 +0530

    Your First 2D Numpy Array

---
+++ NUMPY/LAB2/L1.py
@@ -0,0 +1,8 @@
+"""
+Instructions --
+
+Use np.array() to create a 2D Numpy array from baseball. Name it np_baseball.
+Print out the type of np_baseball.
+Print out the shape attribute of np_baseball. Use np_baseball.shape.
+"""
+
