50b59fee3e04469c62861331491bec212bf7d000
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-06-30 21:38:27 +0530

    2D Arithmetic

---
+++ NUMPY/LAB2/L4.py
@@ -0,0 +1,8 @@
+"""
+Instructions --
+
+You managed to get hold on the changes in weight, height and age of all baseball players. It is available as a 2D Numpy array, update. Add np_baseball and update and print out the result.
+You want to convert the units of height and weight. As a first step, create a Numpy array with three values: 0.0254, 0.453592 and 1. Name this array conversion.
+Multiply np_baseball with conversion and print out the result.
+"""
+
