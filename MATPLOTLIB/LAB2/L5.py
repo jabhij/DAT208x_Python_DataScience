5f664d9b2d94bbab98bee60139e5ba31a1defd09
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-07-04 16:19:34 +0530

    Choose the right plot (2)

---
+++ MATPLOTLIB/LAB2/L5.py
@@ -0,0 +1,6 @@
+You're a professor in Data Analytics with Python, and you want to visually assess if longer answers on exam questions lead to higher 
+grades. Which plot do you use?
+
+Line Chart
+Scateer Plot (CORRECT)
+Histogram
