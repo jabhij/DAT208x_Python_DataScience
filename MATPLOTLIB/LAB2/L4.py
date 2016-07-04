72f04b6b411dc364112ad04ed178d1f845b72a81
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-07-04 16:16:45 +0530

    Choose the right plot (1)

---
+++ MATPLOTLIB/LAB2/L4.py
@@ -0,0 +1,6 @@
+You're a professor teaching Data Science with Python, and you want to visually assess if the grades on your exam follow a normal 
+distribution. Which plot do you use?
+
+Line plot 
+Scatter plot 
+Histogram (CORRECT)
