efa7a967524eb1fddc0e3ed1d66e7ba2d4946cc3
Abhishek Jaiswal <jaisabhii@gmail.com>
2016-07-06 12:04:36 +0530

    Greater and less than

---
+++ PANDAS/LAB1/L2.py
@@ -0,0 +1,20 @@
+"""
+Instructions --
+
+Write Python expressions, wrapped in a print() function, to check whether:
+
+x is greater than or equal to -10. x has already been defined for you.
+"test" is less than or equal to y. y has already been deinfed for you.
+True is greater than False.
+"""
+
+# Comparison of integers
+x = -3 * 6
+print (x >= -10)
+
+# Comparison of strings
+y = "test"
+print ('test' <= y)
+
+# Comparison of booleans
+print (True > False)
